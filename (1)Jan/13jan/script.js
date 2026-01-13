04baaf6dd682916e707ca704ce1acf09082efc56
ayushkas3ra <raun4k@protonmail.com>
2026-01-13 20:45:23 +0530

    13/01/2026

---
+++ (1)Jan/13jan/script.js
@@ -0,0 +1,33 @@
+// console.log('1'-1) is equals to 0 and this is called type coercion
+
+// ---Taking input nums from user and printing it in console---
+
+// let age = prompt("what is ur age?");
+// console.log(age);
+// console.log(typeof age);
+// console.log(typeof Number(age)) It is typecasting or type conversion
+
+// ---Swapping nums---
+
+// method 1( with 3 variables)
+// let num1 = Number(prompt("Enter first number:"));
+// let num2 = Number(prompt("Enter second number:"));
+// let num3;
+// num3 = num1;
+// num1 = num2;
+// num2 = num3;
+// prompt(`First number is ${num1} and second number is ${num2}`);
+
+// Method 2(with 2 variables)
+// let a = 10;
+// let b = 20;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(`The value of a will be ${a} and b is ${b}`);
+
+// Method 3(Destructuring)
+// let a = 10;
+// let b = 20;
+// [a, b] = [b, a];
+// console.log(`The value of a is ${a} and b is ${b}`);
