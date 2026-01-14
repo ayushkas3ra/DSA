7396ab82997662f6e1501cf457e325b3777c2565
ayushkas3ra <raun4k@protonmail.com>
2026-01-14 20:23:28 +0530

    14/01/2026

---
+++ Jan/14jan/script.js
@@ -0,0 +1,66 @@
+// console.log(Math.floor(3564/100))
+
+// console.log(10>2)
+// console.log(10<2)
+// console.log(10>=2)
+// console.log(10<=2)
+// console.log(10!=2)
+// console.log(10==2)
+// console.log(10=='13')
+
+// console.log(10=='10')
+// console.log(10==='10')
+
+// let a = 10;
+// let b = a++;
+// console.log(b);
+// console.log(a);
+
+// let a = 10;
+// let b = ++a;
+// console.log(b);
+// console.log(a);
+
+// let i = 11;
+// i = i++ + ++i;
+// console.log(i);
+
+// let a = 11;
+// let b = 22;
+// let c = a + b + a++ + b++ + ++a  + ++b;
+// console.log('a='+a);
+// console.log('b='+b);
+// console.log('c='+c);
+
+// let b = true;
+// b = b++;
+// console.log(b);
+
+// let b = true;
+// b = ++b;
+// console.log(b);
+
+// console.log(Math.round(10.3));
+// console.log(Math.round(10.5));
+// console.log(Math.ceil(10.3));
+// console.log(Math.floor(10.3));
+// console.log(Math.trunc(10.9));
+// console.log(Math.pow(2, 3));
+// console.log(Math.sqrt(25));
+// console.log(Math.cbrt(27));
+// console.log(Math.abs(-12));
+// console.log(Math.max(38, 28));
+// console.log(Math.min(38, 28));
+// console.log(Math.random());
+// let a = 0.857;
+// console.log(a.toFixed(2))
+
+// Area and perimeter of rectangle
+
+// let length = Number(prompt('Enter length of rectangle:'));
+// let width = Number(prompt('Enter width of rectangle'));
+// alert(`Area of rectangle is ${length*width} and perimeter is ${2*(length+width)}`);
+
+// let r = 12;
+// let c = (2 *(Math.PI)*r).toFixed(2);
+// console.log(c);
